252d8c9a4302309fc6120c00233509116345cb5e
memowii <memogl25@gmail.com>
2020-07-14 20:01:41 -0500

    Add some TODOs

--- src/index.js
+++ src/index.js
@@ -8,3 +8,8 @@ import App from "./components/App";
 import "./global.css";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
+
+// TODO: Refactor code of App.js (try to use hooks, create more components).
+// TODO: Try to apply a undo functionality when deleting counters (redux?).
+// TODO: Drag and drop clickers.
+// TODO: Remove unused css styles (bootstrap and fontawesome?).
